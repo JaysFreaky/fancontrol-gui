ceb8c255f321aad7b4853e4b00cf1bef56761cf9
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-09-04 13:42:44 -0500

    Added ability to run app via apps output

--- flake.nix
+++ flake.nix
@@ -9,9 +9,14 @@
   outputs = { self, nixpkgs, systems }: let
     eachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
+    apps.fancontrol-gui = eachSystem (system: {
+      type = "app";
+      program = "${nixpkgs.lib.getExe' self.packages.${system}.fancontrol-gui "fancontrol_gui"}";
+    });
+
     packages = eachSystem (system: rec {
-      fancontrol-gui = nixpkgs.legacyPackages.${system}.callPackage ./fancontrol.nix { };
       default = fancontrol-gui;
+      fancontrol-gui = nixpkgs.legacyPackages.${system}.callPackage ./fancontrol.nix { };
     });
   };
 }
